3d2833d1e0bf50054f5067960181496d6dbe1f7e
Mai Loan Tran <mailoansaigon@gmail.com>
2023-03-06 07:16:43 +1000

    added Filter tests

--- cypress/e2e/filters.cy.ts
+++ cypress/e2e/filters.cy.ts
@@ -1,7 +1,50 @@
+import { CATEGORY_ID_THOROUGHBRED, CATEGORY_ID_GREYHOUND, CATEGORY_ID_HARNESS } from "../config/constants";
+
+
 describe('Category Filters', () => {
-  it.skip('Insert test here');
+  it('Test selecting Thoroughbred', () => {
+    cy.visit('');
+    cy.get('.item').should('have.length', 5);
+    cy.get('[data-testid=category-filters]').within(() => {
+      [CATEGORY_ID_GREYHOUND, CATEGORY_ID_HARNESS].forEach((category) => {
+        cy.get(`[data-testid=category-filter-${category}]`).within(() => {
+          cy.get('[data-testid=category-filter-checkbox]').should('be.checked');
+          cy.get('[data-testid=category-filter-checkbox]').click();
+          cy.get('[data-testid=category-filter-checkbox]').should('not.be.checked');
+        });
+      })
+    });
+    cy.get('.item').should('have.length', 5);
+  });
 
-  it.skip('Insert test here');
+  it('Test selecting Greyhound', () => {
+    cy.visit('');
+    cy.get('.item').should('have.length', 5);
+    cy.get('[data-testid=category-filters]').within(() => {
+      [CATEGORY_ID_THOROUGHBRED, CATEGORY_ID_HARNESS].forEach((category) => {
+        cy.get(`[data-testid=category-filter-${category}]`).within(() => {
+          cy.get('[data-testid=category-filter-checkbox]').should('be.checked');
+          cy.get('[data-testid=category-filter-checkbox]').click();
+          cy.get('[data-testid=category-filter-checkbox]').should('not.be.checked');
+        });
+      })
+    });
+    cy.get('.item').should('have.length', 5);
+  });
 
-  it.skip('Insert test here');
+  it('Test selecting Harness', () => {
+    cy.visit('');
+    cy.get('.item').should('have.length', 5);
+    cy.get('[data-testid=category-filters]').within(() => {
+      [CATEGORY_ID_THOROUGHBRED, CATEGORY_ID_GREYHOUND].forEach((category) => {
+        cy.get(`[data-testid=category-filter-${category}]`).within(() => {
+          cy.get('[data-testid=category-filter-checkbox]').should('be.checked');
+          cy.get('[data-testid=category-filter-checkbox]').click();
+          cy.get('[data-testid=category-filter-checkbox]').should('not.be.checked');
+        });
+      })
+    });
+    cy.get('.item').should('have.length', 5);
+  });
+
 });
