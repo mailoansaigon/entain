007d3509a21fdec686757bbe941f23b924995a8d
Mai Loan Tran <mailoansaigon@gmail.com>
2023-03-07 11:28:52 +1000

    added Countown timer test

--- cypress/e2e/countdown-timer.cy.ts
+++ cypress/e2e/countdown-timer.cy.ts
@@ -1,7 +1,16 @@
 describe('Countdown Timer', () => {
-  it.skip('Insert test here');
 
-  it.skip('Insert test here');
+  it('Countdown timer should be in correct format', () => {
+    cy.visit('');
+    cy.get('.item').within(() => {
+      cy.get('p').eq(1).invoke('text').should('match', /[\dm]?[\s]?[\ds]?/);
+      cy.get('p').eq(3).invoke('text').should('match', /[\dm]?[\s]?[\ds]?/);
+      cy.get('p').eq(5).invoke('text').should('match', /[\dm]?[\s]?[\ds]?/);
+      cy.get('p').eq(7).invoke('text').should('match', /[\dm]?[\s]?[\ds]?/);
+      cy.get('p').eq(9).invoke('text').should('match', /[\dm]?[\s]?[\ds]?/);
+    });
+    //cy.get('p').should('match', /\dm[ \ds]?/);
 
-  it.skip('Insert test here');
+  });
+
 });
