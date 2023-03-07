0420fb9c88e19a555a2fe68ad5fdd64e675ead4e
Mai Loan Tran <mailoansaigon@gmail.com>
2023-03-07 11:51:33 +1000

    fixed countdown timer test

--- cypress/e2e/countdown-timer.cy.ts
+++ cypress/e2e/countdown-timer.cy.ts
@@ -3,14 +3,20 @@ describe('Countdown Timer', () => {
   it('Countdown timer should be in correct format', () => {
     cy.visit('');
     cy.get('.item').within(() => {
-      cy.get('p').eq(1).invoke('text').should('match', /[\dm]?[\s]?[\ds]?/);
-      cy.get('p').eq(3).invoke('text').should('match', /[\dm]?[\s]?[\ds]?/);
-      cy.get('p').eq(5).invoke('text').should('match', /[\dm]?[\s]?[\ds]?/);
-      cy.get('p').eq(7).invoke('text').should('match', /[\dm]?[\s]?[\ds]?/);
-      cy.get('p').eq(9).invoke('text').should('match', /[\dm]?[\s]?[\ds]?/);
+      // each item have 2 <p> elements, so total number of <p> is 10 max
+      cy.get('p').its('length').should('be.lte', 10);
+      var numElems = cy.get('p').its('length');
+      var countdownRegex = /[\dh]?[\s]?[\dm]?[\s]?[\ds]?/;
+      /*
+      for (var i=1; i < numElems; i = i+2) {
+        cy.get('p').eq(i).invoke('text').should('match', countdownRegex);
+      }*/
+      cy.get('p').eq(1).invoke('text').should('match', countdownRegex);
+      cy.get('p').eq(3).invoke('text').should('match', countdownRegex);
+      cy.get('p').eq(5).invoke('text').should('match', countdownRegex);
+      cy.get('p').eq(7).invoke('text').should('match', countdownRegex);
+      cy.get('p').eq(9).invoke('text').should('match', countdownRegex);
     });
-    //cy.get('p').should('match', /\dm[ \ds]?/);
-
   });
 
 });
